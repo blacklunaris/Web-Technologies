4cf9a7dfd2b1c1d46129e8bbe19e3ce2649ce0e7
blacklunaris <patrickhowardanglin@gmail.com>
2020-11-13 14:38:16 -0500

    first commit

---
+++ react/reactlogin/src/App.js
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from "react";
+import './App.css';
+
+const App= () =>{
+  const APP_ID="3704ea75";
+  const APP_KEY="0f7c23dc8001b3f1e205cb30261985fe";
+
+  useEffect(() =>{
+    getRecipes();
+  },[]);
+
+  const getRecipes= async ()=>{
+    const response= await fetch('https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}');
+    const data= response.json();
+    console.log(data);
+
+    /* Alternatively
+    fetch(https://api.edamam.com)
+    .then(response =>{
+    response.json()
+  })
+    */
+  }
+
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type ="text"/>
+        <button
+          className="search-button"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+
+    </div>
+  );
+
+}
+
+
+export default App;
